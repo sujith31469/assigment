75cd888cb8ea3761a138d9bc60ab518589e352c9
sujith31469 <84025500+sujith31469@users.noreply.github.com>
2021-06-10 19:48:33 +0530

    Add files via upload

---
+++ assignmentjune9.js
@@ -0,0 +1,28 @@
+// to find type of variable
+a="sujith"
+b=30
+console.log(input is-'typeof(a));
+console.log(input is-'typeof(b));
+
+//to find age
+var age=20;
+if(age<3)
+{
+console.log("Infant");
+}
+else if( age>3 && age<12)
+{
+console.log("Child");
+}
+else if( age>12 && age<19)
+{
+console.log("Teen");
+}
+else if( age>19 && age<40)
+{
+console.log("adult");
+}
+else
+{
+console.log("old person");
+}
---
+++ odd-even.js
@@ -0,0 +1,10 @@
+// to find even or odd number
+var num=10;
+if(num%2==0)
+{
+console.log(num +'is even');
+}
+else
+{
+console.log(num +'is odd');
+}
